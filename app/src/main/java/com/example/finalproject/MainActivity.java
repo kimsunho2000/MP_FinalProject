d27accb3ba84d2fdfea67d97ed49c5bc659a9398
kimsunho2000 <sunhokim28@gmail.com>
2024-12-07 01:37:11 +0900

    activity main UI 구현

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.finalproject.ChatActivity;
+import com.example.finalproject.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button serverButton = findViewById(R.id.serverButton);
+        Button clientButton = findViewById(R.id.clientButton);
+
+        serverButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+            intent.putExtra("isServer", true); // 서버 모드 설정
+            startActivity(intent);
+        });
+
+        clientButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+            intent.putExtra("isServer", false); // 클라이언트 모드 설정
+            startActivity(intent);
+        });
+    }
+}
